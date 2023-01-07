1f9c78dcd9c532eebb48aca2d82d5fa66423a6ac
Immernachtreich <ebrahimtohfafarosh11@gmail.com>
2023-01-08 00:29:18 +0530

    Added variables to react expense app

--- React-Guide/src/components/ExpenseItem.js
+++ React-Guide/src/components/ExpenseItem.js
@@ -1,10 +1,22 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return (<div>
-        <h2>Expense Item</h2>
-        <p>Food Rs 10</p>
-        <p>Petrol Rs 100</p>
-        <p>Movies Rs 200</p>
-    </div>);
+
+    const expenseDate = new Date(2016, 2, 28).toLocaleDateString();
+    const expenseTitle = 'Car Insurance';
+    const expensePrice = 69.99;
+    const expenseLocation = 'Insurance Company';
+
+    return (
+        <div className='expense-item'>
+            <div> { expenseDate } </div>
+            <div className='expense-item__description'>
+                <h2> { expenseTitle } </h2>
+                <div className='expense-item__price'> ${ expensePrice } </div>
+                <div> { expenseLocation } </div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
