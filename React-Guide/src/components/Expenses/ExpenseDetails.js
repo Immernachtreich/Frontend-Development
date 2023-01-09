2755fa6b179e21b2d2c4a439071f20e755b0aab0
Immernachtreich <ebrahimtohfafarosh11@gmail.com>
2023-01-09 21:04:13 +0530

    Added event handlers

--- React-Guide/src/components/Expenses/ExpenseDetails.js
+++ React-Guide/src/components/Expenses/ExpenseDetails.js
@@ -1,12 +1,33 @@
+// Package Imports
+import { useState } from 'react';
+
 const ExpenseDetails = props => {
 
-    const { title, location, amount } = props.expense;
+    const { location } = props.expense;
+
+    const [title, setTitle] = useState(props.expense.title);
+    const [amount, setAmount] = useState(props.expense.amount);
+
+    const clickHandler = e => {
+        setTitle('Updated');
+    }
+
+    const deleteHandler = e => {
+        e.target.parentElement.parentElement.remove();
+    }
+
+    const changePriceHandler = e => {
+        setAmount('100');
+    }
 
     return (
         <div className='expense-item__description'>
             <h2> { title } </h2>
             <div className='expense-item__location'> { location } </div>
             <div className='expense-item__price'> ${ amount } </div>
+            <button onClick={ clickHandler }> Change Title </button>
+            <button onClick={ deleteHandler }> Delete Expense </button>
+            <button onClick={ changePriceHandler }> Change Price </button>
         </div>
     );
 }
--- React-Guide/src/components/Expenses/ExpenseItem.js
+++ React-Guide/src/components/Expenses/ExpenseItem.js
@@ -8,20 +8,10 @@ import './ExpenseItem.css';
 
 const ExpenseItem = props => {
 
-    const clickHandler = e => {
-        console.log(e.target.parentElement);
-    }
-
-    const deleteHandler = e => {
-        e.target.parentElement.remove();
-    }
-
     return (
         <Card className='expense-item'>
             <ExpenseDate date={ props.date } />
             <ExpenseDetails expense={ props } />
-            <button onClick={ clickHandler }> Change Title </button>
-            <button onClick={ deleteHandler }> Delete Expense </button>
         </Card>
     );
 }
