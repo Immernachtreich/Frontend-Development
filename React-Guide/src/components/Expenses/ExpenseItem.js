9117116c7890a91614ab562c6c68a3d0cda74397
Immernachtreich <ebrahimtohfafarosh11@gmail.com>
2023-01-09 20:20:25 +0530

    Added buttons and handler functions

--- React-Guide/src/components/Expenses/ExpenseItem.js
+++ React-Guide/src/components/Expenses/ExpenseItem.js
@@ -8,10 +8,20 @@ import './ExpenseItem.css';
 
 const ExpenseItem = props => {
 
+    const clickHandler = e => {
+        console.log(e.target.parentElement);
+    }
+
+    const deleteHandler = e => {
+        e.target.parentElement.remove();
+    }
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={ props.date } />
             <ExpenseDetails expense={ props } />
+            <button onClick={ clickHandler }> Change Title </button>
+            <button onClick={ deleteHandler }> Delete Expense </button>
         </Card>
     );
 }
