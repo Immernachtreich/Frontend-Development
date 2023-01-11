8203c4122f15b8a679a3eeb1f40ab99fdae85022
Immernachtreich <ebrahimtohfafarosh11@gmail.com>
2023-01-11 19:46:35 +0530

    Added expenses dynamically

--- React-Guide/src/App.js
+++ React-Guide/src/App.js
@@ -4,51 +4,66 @@ import { useState } from 'react';
 
 // Component Imports
 import ExpenseItem from './components/Expenses/ExpenseItem';
-import Card from './components/UI/Card';
 import NewExpense from './components/NewExpenses/NewExpense';
+import ExpenseFilter from './components/Expenses/ExpenseFilter';
+
+import Card from './components/UI/Card';
+
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        title: 'Toilet Paper',
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+        location: 'Walmart'
+    },
+    { 
+        id: 'e2', 
+        title: 'New TV', 
+        amount: 799.49, 
+        date: new Date(2021, 2, 12),
+        location: 'Target'
+    },
+    {
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+        location: 'ICICI'
+    },
+    {
+        id: 'e4',
+        title: 'New Desk (Wooden)',
+        amount: 450,
+        date: new Date(2021, 5, 12),
+        location: 'IKEA'
+    },
+];
 
 const App = () => {
-    const expenses = [
-        {
-            id: 'e1',
-            title: 'Toilet Paper',
-            amount: 94.12,
-            date: new Date(2020, 7, 14),
-            location: 'Walmart'
-        },
-        { 
-            id: 'e2', 
-            title: 'New TV', 
-            amount: 799.49, 
-            date: new Date(2021, 2, 12),
-            location: 'Target'
-        },
-        {
-            id: 'e3',
-            title: 'Car Insurance',
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-            location: 'ICICI'
-        },
-        {
-            id: 'e4',
-            title: 'New Desk (Wooden)',
-            amount: 450,
-            date: new Date(2021, 5, 12),
-            location: 'IKEA'
-        },
-    ];
+    
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
     const addExpenseHandler = expense => {
-        console.log(expense);
+        setExpenses(prevExpenses => {
+            return [expense, ...prevExpenses];
+        })
+    }
+
+    const filterChangeHandler =  selectedYear => {
+        setFilteredYear(selectedYear);
     }
 
     return (
         <div>
+            
             <NewExpense onAddExpense={ addExpenseHandler }/>
             <Card className='expenses'>
+                <ExpenseFilter selected={ filteredYear } onChange={ filterChangeHandler } />
                 { expenses.map(e => {
                     return <ExpenseItem
+                        key={ e.id }
                         title={ e.title }
                         amount={ e.amount }
                         date={ e.date }
@@ -56,6 +71,7 @@ const App = () => {
                     />
                 }) }
             </Card>
+
         </div>
     );
 }
--- React-Guide/src/components/Expenses/ExpenseDetails.js
+++ React-Guide/src/components/Expenses/ExpenseDetails.js
@@ -25,9 +25,9 @@ const ExpenseDetails = props => {
             <h2> { title } </h2>
             <div className='expense-item__location'> { location } </div>
             <div className='expense-item__price'> ${ amount } </div>
-            <button onClick={ clickHandler }> Change Title </button>
+            {/* <button onClick={ clickHandler }> Change Title </button>
             <button onClick={ deleteHandler }> Delete Expense </button>
-            <button onClick={ changePriceHandler }> Change Price </button>
+            <button onClick={ changePriceHandler }> Change Price </button> */}
         </div>
     );
 }
